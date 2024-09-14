6101665b6912218f210927e98a7d3d5715ad0bdd
Тимофей Соболев <tim@MacBook-Pro-Timofej.local>
2024-09-14 12:45:58 +0300

    added handlers for GET, POST, DELETE for endpoint /tasks

--- precode.go
+++ precode.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -39,14 +41,101 @@ var tasks = map[string]Task{
 	},
 }
 
-// Ниже напишите обработчики для каждого эндпоинта
-// ...
+// обработка метода GET для эндпоинта `/tasks`
+// обработчик должен вернуть все задачи, которые хранятся в мапе.
+// При успешном запросе сервер должен вернуть статус 200 OK.
+// При ошибке сервер должен вернуть статус 500 Internal Server Error.
+func getTasks(w http.ResponseWriter, r *http.Request) {
+	resp, err := json.Marshal(tasks)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+// обработка метода POST для эндпоинта `/tasks`
+// обработчик должен принимать задачу в теле запроса и сохранять ее в мапе.
+// При успешном запросе сервер должен вернуть статус 201 Created.
+// При ошибке сервер должен вернуть статус 400 Bad Request.
+func postTask(w http.ResponseWriter, r *http.Request) {
+	var task Task
+	var buf bytes.Buffer
+
+	_, err := buf.ReadFrom(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err = json.Unmarshal(buf.Bytes(), &task); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tasks[task.ID] = task
+
+	w.Header().Set("Content-Type", "applicaion/json")
+	w.WriteHeader(http.StatusCreated)
+}
+
+// обработка метода GET для эндпоинта `/tasks/{id}`
+// обработчик должен вернуть задачу с указанным в запросе пути ID, если такая есть в мапе
+// при успешном выполнении запроса сервер должен вернуть статус 200 OK.
+// в случае ошибки или отсутствия задачи в мапе сервер должен вернуть статус 400 Bad Request
+func getTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	task, ok := tasks[id]
+	if !ok {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	resp, err := json.Marshal(task)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(resp)
+}
+
+// обработка метода DELETE для эндпоинта `/tasks/{id}`
+// обработчик должен удалить задачу из мапы по её ID
+// при успешном выполнении запроса сервер должен вернуть статус 200 OK
+// в случае ошибки или отсутствия задачи в мапе сервер должен вернуть статус 400 Bad Request
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	_, ok := tasks[id]
+	if !ok {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	delete(tasks, id)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	r := chi.NewRouter()
 
-	// здесь регистрируйте ваши обработчики
-	// ...
+	// регистрируем в роутере эндпоинт `/tasks` с методом GET
+	r.Get("/tasks", getTasks)
+	// регистрируем в роутере эндпоинт `/tasks` с методом POST
+	r.Post("/tasks", postTask)
+	// регистрируем в роутере эндпоинт `/tasks/{id}` с методом GET
+	r.Get("/tasks/{id}", getTask)
+	// регистрируем в роутере эндпоинт `/tasks/{id}` с методом DELETE
+	r.Delete("/tasks/{id}", deleteTask)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		fmt.Printf("Ошибка при запуске сервера: %s", err.Error())
